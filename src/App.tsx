ac3ab9bf25bb713321144f900b32cdf7bddf4e39
Lavi <lavi_fang@proton.me>
2023-09-23 17:12:04 +0800

    add repo link

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,19 @@ import Editor from './editor';
 
 function App() {
   return (
-    <div className="card">
-      <Editor />
-    </div>
+    <>
+      <div>
+        <a
+          href="https://github.com/Lauviah0622/rich-text-md-editor"
+          target="_blank"
+        >
+          Repo link
+        </a>
+      </div>
+      <div className="card">
+        <Editor />
+      </div>
+    </>
   );
 }
 
